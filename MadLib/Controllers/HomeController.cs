6cd28e94c3f6d90925cc290232e9c266670e08ee
Tanvi Garg and Victoria Martinez <victoria@>
2018-11-26 14:12:35 -0800

    add new madlibs properties, form fields and restructure result page

--- MadLib/Controllers/HomeController.cs
+++ MadLib/Controllers/HomeController.cs
@@ -9,12 +9,17 @@ namespace MadLib.Controllers
     public ActionResult Form() { return View(); }
 
     [Route("/madlib")]
-    public ActionResult MadLibResult(string noun, string verb, string adjective)
+    public ActionResult MadLibResult(string noun, string nounOne, string verb, string pastTenseVerb, string adjective, string adjectiveOne, string typeOfBird, string roomInAHouse)
     {
       WordsLib myWordsLib = new WordsLib();
       myWordsLib.SetNoun(noun);
+      myWordsLib.SetNounOne(nounOne);
       myWordsLib.SetVerb(verb);
+      myWordsLib.SetPastTenseVerb(pastTenseVerb);
       myWordsLib.SetAdjective(adjective);
+      myWordsLib.SetAdjectiveOne(adjectiveOne);
+      myWordsLib.SetTypeofBird(typeOfBird);
+      myWordsLib.SetRoomInAHouse(roomInAHouse);
       return View(myWordsLib);
     }
 
--- MadLib/Models/WordsLib.cs
+++ MadLib/Models/WordsLib.cs
@@ -3,9 +3,13 @@ namespace MadLib.Models
   public class WordsLib
   {
     private string _noun;
+    private string _nounOne;
     private string _verb;
     private string _adjective;
-    // private string _partOfBody;
+    private string _adjectiveOne;
+    private string _typeOfBird;
+    private string _roomInAHouse;
+    private string _pastTenseVerb;
 
     public string GetNoun()
     {
@@ -17,6 +21,15 @@ namespace MadLib.Models
       _noun = newNoun;
     }
 
+    public string GetNounOne()
+    {
+      return _nounOne;
+    }
+
+    public void SetNounOne(string newNounOne)
+    {
+      _nounOne = newNounOne;
+    }
     public string GetVerb()
     {
       return _verb;
@@ -25,6 +38,16 @@ namespace MadLib.Models
     public void SetVerb(string newVerb)
     {
       _verb = newVerb;
+    }
+
+    public string GetPastTenseVerb()
+    {
+      return _pastTenseVerb;
+    }
+
+    public void SetPastTenseVerb(string newPastTenseVerb)
+    {
+      _pastTenseVerb = newPastTenseVerb;
     }
 
     public string GetAdjective()
@@ -37,6 +60,35 @@ namespace MadLib.Models
       _adjective = newAdjective;
     }
 
+    public string GetAdjectiveOne()
+    {
+      return _adjectiveOne;
+    }
+
+    public void SetAdjectiveOne(string newAdjectiveOne)
+    {
+      _adjectiveOne = newAdjectiveOne;
+    }
+
+    public string GetTypeofBird()
+    {
+      return _typeOfBird;
+    }
+
+    public void SetTypeofBird(string newTypeOfBird)
+    {
+      _typeOfBird = newTypeOfBird;
+    }
+
+    public string GetRoomInAHouse()
+    {
+      return _roomInAHouse;
+    }
+
+    public void SetRoomInAHouse(string newRoomInAHouse)
+    {
+      _roomInAHouse = newRoomInAHouse;
+    }
 
   }
 }
