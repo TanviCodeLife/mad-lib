5834058d1d8bf4e772d3df0ff1859decd328af86
Tanvi Garg and Victoria Martinez <tanvi@>
2018-11-26 14:37:53 -0800

    add functionality to select between two theme of madlibs

--- MadLib/Controllers/HomeController.cs
+++ MadLib/Controllers/HomeController.cs
@@ -6,10 +6,31 @@ namespace MadLib.Controllers
   public class HomeController : Controller
   {
     [Route("/")]
+    public ActionResult Index() { return View(); }
+
+    [Route("/form")]
     public ActionResult Form() { return View(); }
 
     [Route("/madlib")]
     public ActionResult MadLibResult(string noun, string nounOne, string verb, string pastTenseVerb, string adjective, string adjectiveOne, string typeOfBird, string roomInAHouse)
+    {
+      WordsLib myWordsLib = new WordsLib();
+      myWordsLib.SetNoun(noun);
+      myWordsLib.SetNounOne(nounOne);
+      myWordsLib.SetVerb(verb);
+      myWordsLib.SetPastTenseVerb(pastTenseVerb);
+      myWordsLib.SetAdjective(adjective);
+      myWordsLib.SetAdjectiveOne(adjectiveOne);
+      myWordsLib.SetTypeofBird(typeOfBird);
+      myWordsLib.SetRoomInAHouse(roomInAHouse);
+      return View(myWordsLib);
+    }
+
+    [Route("/form-one")]
+    public ActionResult FormOne() { return View(); }
+
+    [Route("/madlib-one")]
+    public ActionResult MadLibResultOne(string noun, string nounOne, string verb, string pastTenseVerb, string adjective, string adjectiveOne, string typeOfBird, string roomInAHouse)
     {
       WordsLib myWordsLib = new WordsLib();
       myWordsLib.SetNoun(noun);
